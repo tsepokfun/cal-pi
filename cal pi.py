48de076953588a5992360699634231bd6a28c1cd
tsepokfun <97015774+tsepokfun@users.noreply.github.com>
2024-05-23 07:44:15 +0800

    Update cal pi.py

--- cal pi.py
+++ cal pi.py
@@ -1,17 +1,20 @@
 import random
+from decimal import Decimal
+import sys
+sys.set_int_max_str_digits(0)
 
 #let square = 2 * 2
 #let r = 1
 
-InC = 0
-OutC = 0
+InC = Decimal(“0”)
+OutC = Decimal(“0“)
 
-for i in range(1000000000) :
+while 1 :
     t1 = random.random()
     t2 = random.random()
     if (abs(t1 - 1) ** 2 + abs(t2 - 1) ** 2) ** 0.5 < 1 :
         InC += 1
     else :
         OutC += 1
-
-print(((InC / (InC + OutC)) * 4))
+    if InC + OutC % 573647 :
+    print(((InC / (InC + OutC)) * 4), str(InC +OutC))
